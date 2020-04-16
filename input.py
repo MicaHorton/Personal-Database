b854f56ef9bbc21bb4bf1236aef392322fb3fe8c
Mica Horton <mica.e.horton@gmail.com>
2020-04-16 09:32:48 -0700

    Delete unnecessary comments

--- input.py
+++ input.py
@@ -31,53 +31,5 @@ with open(database,'r') as f:
 with open(database, 'w') as f:
     json.dump(data, f, indent=2) 
 
-# Create File
-# Add Tags
-# Add Timestamp
-# Option 1: Create JSON Database with link to file with tags and timestamp. Ahh, this also solves the encryption problem! Fantastic.
-# Option 2: Include tags & timestamp in header of file (maybe create a new file type with metadata?)
 
 
-
-
-'''
-thought = input()
-
-timestamp = datetime.datetime.now()
-timestamp = timestamp.strftime('%a %x %X')
-
-print('Add tags?', end=' ')
-tags = input()
-tags = re.findall('[^,\s][^\,]*[^,\s]*', tags)
-
-newInstance = {
-    'thought': thought,
-    'timestamp': timestamp,
-    'tags': tags
-}
-
-subprocess.run('nano')
-
-
-
-
-
-file = '/Users/mica/Projects/PDB/data.json'
-with open(file,'r') as f:
-    data = json.load(f)
-    data.append(newInstance)
-with open(file, 'w') as f:
-    json.dump(data, f, indent=2) 
-
-import os
-import shutil
-import subprocess
-
-output_file = join_file.replace('.txt','.mp4')
-createMP4 = 'ffmpeg -f concat -safe 0 -i {join_file} -c copy {output_file}'.format(join_file=join_file, output_file=output_file)
-subprocess.run(createMP4, shell=True)
-
-os.remove(join_file)
-shutil.rmtree(path)
-
-'''
