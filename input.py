e8ce3cd7b2cec16dd53608a25eaaf0a37740c99c
Mica Horton <mica.e.horton@gmail.com>
2020-04-16 09:31:29 -0700

    Use files instead of strings
    Close: #1

--- input.py
+++ input.py
@@ -1,7 +1,46 @@
+import subprocess
 import json
 import datetime
 import re
+import time
 
+timestamp = datetime.datetime.now()
+timestamp = timestamp.strftime('%a %x %X').replace('/','.')
+
+fileDirectory = '/Users/mica/Projects/PDB/Files/'
+fileName = timestamp
+
+command = 'nano ' + '\'' + fileDirectory + fileName + '\''
+subprocess.run(command, shell=True)
+
+print('Add tags?', end=' ')
+tags = input()
+tags = re.findall('[^,\s][^\,]*[^,\s]*', tags)
+
+filePath = '/Users/mica/Projects/PDB/Files/' + fileName
+newInstance = {
+    'file': filePath,
+    'timestamp': timestamp,
+    'tags': tags
+}
+
+database = '/Users/mica/Projects/PDB/dataBase.json'
+with open(database,'r') as f:
+    data = json.load(f)
+    data.append(newInstance)
+with open(database, 'w') as f:
+    json.dump(data, f, indent=2) 
+
+# Create File
+# Add Tags
+# Add Timestamp
+# Option 1: Create JSON Database with link to file with tags and timestamp. Ahh, this also solves the encryption problem! Fantastic.
+# Option 2: Include tags & timestamp in header of file (maybe create a new file type with metadata?)
+
+
+
+
+'''
 thought = input()
 
 timestamp = datetime.datetime.now()
@@ -17,6 +56,12 @@ newInstance = {
     'tags': tags
 }
 
+subprocess.run('nano')
+
+
+
+
+
 file = '/Users/mica/Projects/PDB/data.json'
 with open(file,'r') as f:
     data = json.load(f)
@@ -24,3 +69,15 @@ with open(file,'r') as f:
 with open(file, 'w') as f:
     json.dump(data, f, indent=2) 
 
+import os
+import shutil
+import subprocess
+
+output_file = join_file.replace('.txt','.mp4')
+createMP4 = 'ffmpeg -f concat -safe 0 -i {join_file} -c copy {output_file}'.format(join_file=join_file, output_file=output_file)
+subprocess.run(createMP4, shell=True)
+
+os.remove(join_file)
+shutil.rmtree(path)
+
+'''
