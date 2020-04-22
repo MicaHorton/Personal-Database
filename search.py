f5ebf210e7e6166dec8bca12558fffa82a4365c1
Mica Horton <mica.e.horton@gmail.com>
2020-04-22 11:56:18 -0700

    Add search feature
    Close: #3

---
+++ search.py
@@ -0,0 +1,77 @@
+# Note: Has to be run from Code folder
+
+# Decrypt Files btw
+
+
+# Create/open index
+import os.path
+from whoosh.fields import Schema, TEXT, ID, KEYWORD
+from whoosh import index
+
+schema = Schema(content=TEXT, path=ID(stored=True), tags=KEYWORD(scorable=True))
+
+os.chdir('..')
+#print(os.getcwd())
+if not os.path.exists('Index'):
+    os.mkdir('Index')
+    ix = index.create_in("Index", schema)
+else:
+    ix = index.open_dir("Index")
+
+
+# Add new files to index
+import json
+database = '/Users/mica/Projects/PDB/dataBase.json'
+writer = ix.writer()
+
+with open(database,'r') as f:
+    data = json.load(f)
+    for item in data:
+        if item['indexed'] == False:
+        
+            with open(item['file']) as f:
+                content = f.read()   
+
+            keywords = ''
+            for word in item['tags']:
+                keywords = keywords + word + ' '
+
+            writer.add_document(content=content, path=item['file'], tags=keywords)
+            item['indexed'] = True
+
+with open(database, 'w') as f:
+    json.dump(data, f, indent=2)
+
+writer.commit()
+
+
+
+# Alow user to search
+from whoosh.qparser import QueryParser
+from whoosh.highlight import UppercaseFormatter
+
+with ix.searcher() as searcher:
+    print('Enter search phrase:', end=' ')
+    querystring = input()
+
+    parser = QueryParser('content', ix.schema)
+    query = parser.parse(querystring)
+    results = searcher.search(query)
+
+    results.formatter = UppercaseFormatter()
+    def prCyan(skk): print("\033[96m{}\033[00m" .format(skk))
+ 
+    for hit in results:
+        with open(hit['path']) as f:
+            contents = f.read()
+       
+        fragment = hit.highlights('content', text=contents)
+        print('\n' + fragment)
+        prCyan(hit['path'])
+        
+
+        
+        #print('{fragment:100}{path:10}'.format(fragment=fragment, path=hit['path']))
+
+
+        
